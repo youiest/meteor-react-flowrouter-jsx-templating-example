062ae4046562b510289e343c37399bc13ada4dfe
wiber <elias@youiest.com>
2015-08-24 21:17:33 +0400

    replace given

--- tests/cucumber/features/landing-page/curate_a_landing_page.js
+++ tests/cucumber/features/landing-page/curate_a_landing_page.js
@@ -1,18 +1,19 @@
 module.exports = function () {
+  this.Given( /^I have created a landing page with the heading "([^"]*)"$/, function ( heading ) {
+    return this.server.call( 'page/create', {
+      path: '/',
+      heading: heading
+    } );
+  } );
 
-  this.Given(/^I have created a landing page with the heading "([^"]*)"$/, function (arg1, callback) {
+  this.When( /^a user navigates to the landing page$/, function ( callback ) {
     // Write code here that turns the phrase above into concrete actions
     callback.pending();
-  });
+  } );
 
-  this.When(/^a user navigates to the landing page$/, function (callback) {
+  this.Then( /^they see the heading "([^"]*)"$/, function ( arg1, callback ) {
     // Write code here that turns the phrase above into concrete actions
     callback.pending();
-  });
-
-  this.Then(/^they see the heading "([^"]*)"$/, function (arg1, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback.pending();
-  });
+  } );
 
 };
