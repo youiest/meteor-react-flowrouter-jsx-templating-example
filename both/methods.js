d89d96b71b8d904316712b7ccab21a14ecbb32ce
wiber <elias@youiest.com>
2015-08-25 18:38:14 +0400

    works with server method call

--- both/methods.js
+++ both/methods.js
@@ -2,8 +2,31 @@ Meteor.methods({
   'resetPosts': function () {
     Posts.remove({});
   },
-  'posts/create': function (page) {
+  'posts/create': function (post) {
+
+    Posts.insert(post);
+  },
+  'posts/createDefault': function () {
 // tests call them pages... landing post?!
-    Posts.insert(page);
+    a = {
+      _id: 'one',
+      title: 'New Meteor Rocks',
+      content: 'Yeah! Check our Meteor Blog for more!',
+      category: 'landingPost'
+    }
+    Posts.insert(a);
+    b = {
+      _id: 'two',
+      title: 'MeteorHacks + Kadira => Kadira++',
+      content: 'Something new soon.',
+      category: 'landingPost'
+    }
+    Posts.insert(b);
+    c = {
+      _id: 'three',
+      title: 'My Secret Post',
+      category: 'private'
+    };
+    Posts.insert(c);
   }
 });
