c2ba173a751ebb7c7488b41c845b1b8db7df06b1
wiber <elias@youiest.com>
2015-08-25 16:48:18 +0400

    going Noa and making two from anything

--- tests/cucumber/features/support/hooks.js
+++ tests/cucumber/features/support/hooks.js
@@ -2,4 +2,10 @@ module.exports = function () {
   this.Before(function () {
     return this.server.call('reset');
   });
+  this.Before(function () {
+    return this.server.call('resetPosts');
+  });
+  this.Before(function () {
+    return this.server.call('post/create','REACT! You FROM ... got TO ... and do what comes natural!');
+  });
 };
--- tests/cucumber/fixtures/fixtures.js
+++ tests/cucumber/fixtures/fixtures.js
@@ -1,45 +1,46 @@
-Meteor.methods({
+Meteor.methods( {
   'reset': function () {
-    Pages.remove({});
+    Pages.remove( {} );
   },
-  'page/create': function (page) {
-    Pages.insert(page);
-  }
-});
-
-
-caller = function call(name, payload) {
-  Meteor.call(name, name, payload, function (error, result) {
-    if (error) {
-      log.error("error", error);
-    }
-    if (result) {
-      return (result)
-    }
-  });
-}
-Meteor.methods({
-  'post/create': function (name, page) {
+  'resetPosts': function () {
+    Posts.remove( {} );
+  },
+  'page/create': function ( page ) {
+    Pages.insert( page );
+  },
+  'post/create': function ( name, page ) {
     name = name || 'addPost'
-    log = loglevel.createLogger(name);
-    Posts.insert({
+    log = loglevel.createLogger( name );
+    Posts.insert( {
       _id: 'My Blog',
       title: 'My Blog',
       content: page
-    });
+    } );
 
-    if (Meteor.isClient) {
+    if ( Meteor.isClient ) {
 
       try {
-        log.trace(name + ' isClient');
-      } catch (e) {
-        throw new Meteor.Error("bad-page", e);
+        log.trace( name + ' isClient' );
+      } catch ( e ) {
+        throw new Meteor.Error( "bad-page", e );
       } finally {}
 
     }
-    if (Meteor.isServer) {
-      log.trace(name + ' isServer');
+    if ( Meteor.isServer ) {
+      log.trace( name + ' isServer' );
       return 'serverReturned'
     }
   }
-});
+} );
+
+
+caller = function call( name, payload ) {
+  Meteor.call( name, name, payload, function ( error, result ) {
+    if ( error ) {
+      log.error( "error", error );
+    }
+    if ( result ) {
+      return ( result )
+    }
+  } );
+}
