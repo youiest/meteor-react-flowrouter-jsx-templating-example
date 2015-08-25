52d17f6505fdc2288474f8df3b1cbf19ffda5b40
wiber <elias@youiest.com>
2015-08-25 17:54:59 +0400

    no methods :(

--- both/collections.jsx
+++ both/collections.jsx
@@ -3,27 +3,26 @@ Pages = new Mongo.Collection('pages');
 log = loglevel.createLogger('ssr.collections', 'trace');
 if (Meteor.isServer) {
   Posts.remove({});
-  p = []
+
   a = {
     _id: 'one',
     title: 'New Meteor Rocks',
-    content: 'Yeah! Check our Meteor Blog for more!'
+    content: 'Yeah! Check our Meteor Blog for more!',
+    category: 'landingPost'
   }
-  //Posts.insert(a);
+  Posts.insert(a);
   b = {
     _id: 'two',
     title: 'MeteorHacks + Kadira => Kadira++',
     content: 'Something new soon.'
+    category: 'landingPost'
   }
-  //Posts.insert(b);
+  Posts.insert(b);
   c = {
     _id: 'three',
     title: 'My Secret Post',
     category: 'private'
   };
-  //Posts.insert(c);
-  var po = {}
-  for (var i in [a,b,c]){
-    Posts.insert(i)
-  }
+  Posts.insert(c);
+
 }
